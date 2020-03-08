ec3d06a1c6116e33d6d8408c366c62044cc6601a
Cyclo9 <liang.aidan139@gmail.com>
2020-03-07 19:54:08 -0800

    Changed rectangles to an orb

--- script.py
+++ script.py
@@ -3,6 +3,7 @@
 import os, sys
 import pygame
 from pygame.locals import *
+
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -15,11 +16,13 @@ win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Flashtime!")
 
 # Variables
-rect1_x = 50
-rect1_y = 50
+orb_x = 250
+orb_y = 250
 
-rect2_x = 110
-rect2_y = 50
+""" FUNCTIONS """
+
+def drawOrb():
+    pygame.draw.circle(win, (0, 255, 255), (orb_x, orb_y), 25)
 
 """ MAIN LOOP """
 
@@ -44,69 +47,41 @@ while status:
     # Removes remanants of drawings
     win.fill((0, 0, 0))
 
-    # Draws a rectangle --> rect1
-    pygame.draw.rect(win, (255, 255, 255), (rect1_x, rect1_y, 50, 50))
-    
-    # Draws a 2nd rectangle --> rect2
-    pygame.draw.rect(win, (0, 225, 255), (rect2_x, rect2_y, 50, 50))
+    # Creats Blue Orb --> Main Player
+    drawOrb()
 
     # Velocities for moving objects
-    rect1Vel = 1
-    rect2Vel = 1
+    orbVel = 1
 
     """ MOVEMENT """
 
-    # Keys for moving rectangle 1
-
-    if keys[K_LEFT]:
-        rect1_x -= rect1Vel
-    if keys[K_RIGHT]:
-        rect1_x += rect1Vel
-    if keys[K_DOWN]:
-        rect1_y += rect1Vel
-    if keys[K_UP]:
-        rect1_y -= rect1Vel
-
-    # Keys for moving rectangle 2
-
     if keys[K_w]:
-        rect2_y -= 1
+        orb_y -= orbVel
     if keys[K_a]:
-        rect2_x -= 1
+        orb_x -= orbVel
     if keys[K_s]:
-        rect2_y += 1
+        orb_y += orbVel
     if keys[K_d]:
-        rect2_x += 1
+        orb_x += orbVel
 
     """ BOUNDARIES """
 
-    # Boundaries for rectangle 1
+    if orb_x > 450:
+        orb_x = 450
+    if orb_x < 0:
+        orb_x = 0
+    if orb_y > 450:
+        orb_y = 450
+    if orb_y < 0:
+        orb_y = 0
 
-    if rect1_x > 450:
-        rect1_x = 450
-    if rect1_y > 450:
-        rect1_y = 450
-    if rect1_x < 0:
-        rect1_x = 0
-    if rect1_y < 0:
-        rect1_y = 0
-
-    # Boundaries for rectangle 2
-
-    if rect2_x > 450:
-        rect2_x = 450
-    if rect2_y > 450:
-        rect2_y = 450
-    if rect2_x < 0:
-        rect2_x = 0
-    if rect2_y < 0:
-        rect2_y = 0
+    """ WRAP UP """
 
     # Shows the drawings, after they are made3
     pygame.display.update()
 
     """ STATS """
 
-    print("(" + str(rect1_x) + ", " + str(rect1_y) + ")" + " " + "(" + str(rect2_x) + ", " + str(rect2_y) + ")") # Shows coordinates of rect1
+    print("(" + str(orb_x) + ", " + str(orb_y) + ")") # Shows coordinates of the orb
 
 pygame.quit()
